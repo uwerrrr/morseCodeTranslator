a1aac674c6c6b30aed0dd189f54ecd3591083dcd
Oscar (Van NGUYEN) <32070259+uwerrrr@users.noreply.github.com>
2023-06-25 16:43:53 +1000

    finish core functions

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,64 +2,129 @@ import * as data from "./modules/data.js";
 import * as dom from "./modules/dom-utils.js";
 import * as ut from "./modules/unitTest.js";
 
-const testLatin = "SOS test";
-const testMorse = "... --- ...       - . ... -";
-
-const WoToMoDataCharKeys = Object.keys(data.WoToMoData);
-// ["A","B","C"]
-const MoToWoDataCharKeys = Object.keys(data.MoToWoData);
-// [".-", "-...", "-.-."]
-
-// const translateWoToMo = (latinWords) => {
-//   const latinChars = latinWords.toUpperCase().split("");
-//   // [S,O,S, ,T,E,S,T]
-//   const morseChars = latinChars.map((latinChar) => {
-//     // latinChar = "S"
-//     const morseChar = WoToMoDataCharKeys.reduce((acc, charKey) => {
-//       // ["A"] in ["A","B","C"]
-//       if (latinChar === charKey) {
-//         acc = data.WoToMoData[charKey];
-//       } else if (latinChar === " ") {
-//         acc = latinChar + latinChar; // 2 spaces
-//       }
-//       return acc;
-//     }, "");
-//     return morseChar;
-//   });
-//   return morseChars.join(" "); // join with space
-// };
-
-// const translateMoToWo = (morseWords) => {
-//   const morseChars = morseWords.split(" ");
-//   // [...,---,...,  ,-,.,...,-]
-//   console.log("morseChars: ", morseChars);
-//   const latinChars = morseChars.map((morseChar) => {
-//     // morseChar = "..."
-//     const latinChar = MoToWoDataCharKeys.reduce((acc, charKey) => {
-//       // [".-"] in [".-", "-...", "-.-."]
-//       if (morseChar === charKey) {
-//         acc = data.MoToWoData[charKey];
-//       } else if (morseChar === "  ") {
-//         // 2 spaces
-//         acc = " "; // 1 space
-//       }
-//       return acc;
-//     }, "");
-//     return latinChar;
-//   });
-//   return latinChars.join(""); // join with space
-// };
-
-// console.log("word to morse: ", translateWoToMo(testLatin));
-// console.log("morse to word: ", translateMoToWo(testMorse));
+const testLatin = "S OS     test";
+const testMorse = "...      --- ... / - . ... -";
 
 // const regex = /[A-Z]/g; // /[A-Z0-9]/g;
 // const regex = /\S+/g;
 // console.log("test: ", testMorse.toUpperCase().split("    ")[1].split(" "));
 
-const inputString =
-  "... --- ...     - . ... -    word1     word2   word3    word4    ...";
-const regexPattern = /\b\S+(?:\s{3,}\S+)*\b/g;
-const matches = inputString.match(regexPattern);
+// function removing blank item in an array
+const removeBlankItem = (arr) => {
+  const cleanArr = arr.filter((item) => item !== "");
+  return cleanArr;
+};
 
-console.log(matches);
+// function getting value in Obj
+const getValFromObj = (keyToFind, Obj) => {
+  const objKeys = Object.keys(Obj); // array of keys
+  if (!objKeys.includes(keyToFind)) {
+    throw new Error(`"${keyToFind}" is not a valid input`);
+  }
+  const foundVal = objKeys.reduce((acc, key) => {
+    if (keyToFind === key) {
+      acc = Obj[key];
+    }
+    return acc;
+  }, ""); // return val in string
+
+  return foundVal;
+};
+
+// function translating char by char of an array of chars
+const translateChars = (toTranslateChars, data) => {
+  const translatedChars = toTranslateChars.map((char) => {
+    if (char) {
+      // if NOT ''
+      const translatedChar = getValFromObj(char, data);
+      return translatedChar;
+    } else {
+      return char;
+    }
+  });
+  // return Array of translated char
+
+  const translatedCharsClean = removeBlankItem(translatedChars);
+
+  return translatedCharsClean;
+};
+
+// function translating Morse to Words
+const translateMoToWo = (morseSen) => {
+  const morseWords = morseSen.split("/"); //['...  --- ...' , '- . ... -']
+  const latinWords = morseWords.map((morseWord) => {
+    const morseChars = morseWord.split(" ");
+    // ['...', '', '---', '...' ]
+
+    const latinChars = translateChars(morseChars, data.MoToWoData);
+    // ['S', '', 'O', 'S']
+
+    const latinWord = latinChars.join("");
+    // 'SOS'
+
+    return latinWord;
+  });
+  // returns Array of ['SOS', 'TEST']
+
+  const latinSen = latinWords.join(" ");
+  // 'SOS TEST'
+
+  return latinSen;
+};
+// 'SOS TEST'
+
+// function translating Words to Morse
+const translateWoToMo = (latinSen) => {
+  // `SOS     test`
+  const latinWords = latinSen.toUpperCase().split(" ");
+  //[ 'SOS', '', '', '', '', 'TEST' ]
+
+  const latinWordsClean = removeBlankItem(latinWords);
+
+  const morseWords = latinWordsClean.map((latinWord) => {
+    const latinChars = latinWord.split("");
+    // ['S', ' ', 'O', 'S']
+
+    const morseChars = translateChars(latinChars, data.WoToMoData);
+    // ['...', '---', '...' ]
+
+    const morseWord = morseChars.join(" ");
+    // '... --- ...'
+
+    return morseWord;
+  });
+  // returns Array of [''... --- ...'', '- . ... -']
+
+  const morseSen = morseWords.join(" / ");
+  // '...   --- ... / - . ... -'
+
+  return morseSen;
+};
+// '...   --- ... / - . ... -'
+
+/////////////////////
+//// testing area
+//
+
+// const inputString =
+//   "... --- ...     - . ... -    word1     word2   word3    word4    ...";
+// const regexPattern = /\b\S+(?:\s{3,}\S+)*\b/g;
+// const matches = inputString.match(regexPattern);
+
+// console.log(matches);
+
+// try {
+//   let testResult = getValFromObj(")", data.WoToMoData);
+//   console.log("testResult ", testResult);
+// } catch (e) {
+//   console.log("e.message  ", e.message);
+// }
+
+// console.log(".-.   -- . / ..- -".split("/"));
+// console.log(".-.   -- . / ..- -".split("/")[0].split(" "));
+
+console.log("translate ", testMorse);
+console.log(translateMoToWo(testMorse));
+
+console.log("translate ", testLatin);
+console.log(translateWoToMo(testLatin));
